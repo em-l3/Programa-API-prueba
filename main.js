0f399adedc2577b1c5ac59a04bb75848260c2699
Em1l-3 <elorences@timware.com.mx>
2022-12-22 17:24:13 -0600

    Se agrego region

--- main.js
+++ main.js
@@ -6,21 +6,25 @@ const respuesta=document.querySelector('#respuesta');
 
 const muestraResultados=(res)=>{
     const capital=res[0].capital;
-    respuesta.innerHTML=`<p>${capital}</p>`
+    const region=res[0].region;
+    respuestaNombre.innerHTML=`<p>${capital}</p>`
+    respuestaRegion.innerHTML=`<p>${region}</p>`
 }
 
 const recibeInfo=async()=>{
-    const buscaPais=busqueda.value;
+    const buscaPais=busqueda.value; //
     const endpoint=`${url}${buscaPais}`;
+
 
     try{
         //Verificar pais ingresado
         const response = await fetch(endpoint,{cache:'no-cache'});
-        //Si coincide con la api
+        //Si pais coincide con api
         if(response.ok){
             const jsonResponse=await response.json();
 
             muestraResultados(jsonResponse);
+        //Sino encuentra el pais
         }else{
             respuesta.innerHTML=`<p>No encontrado</p>`
         }
